7c89a137cc1ff6befae6b7e2df99991dbdbd48a9
MaYaSeVeN <mayaseven@hotmail.com>
2013-08-07 00:03:14 +0700

    Update hacking_workshop.py

--- hacking_workshop.py
+++ hacking_workshop.py
@@ -2,23 +2,38 @@
 #Author: MaYaSeVeN
 import random, time, urllib2, json
 
-#candidates = [line.strip() for line in open("candidates.txt", 'r')]
+print "\n!!! All of Candidates for Hacking Workshop 17-18 Augest 2013 !!!\n"
 
-print "\n!!! All of Candidates for Hacking Workshop 6-7 April 2013 !!!\n"
+accessToken = "[Facebook_Access_Token]" #Censor
 
-candidatesFromFacebook = ["https://graph.facebook.com/459003187504945/likes?limit=1000&offset=0", "https://graph.facebook.com/150687625092061/likes?limit=1000&offset=0"]
+posts = ["506599602745303", "503546013050662"]
+
 candidates = []
 
-for candidatesFromPost in candidatesFromFacebook:
-	candidatesJson = json.load(urllib2.urlopen(candidatesFromPost))
-	for candidatesNumber in range(len(candidatesJson["data"])):
-		nameOfCandidate = candidatesJson["data"][candidatesNumber]["name"]
-		candidates.append(nameOfCandidate)
+for postID in posts:
+    candidatesLike = []
+    candidatesShare = []
 
-for candidate in candidates:		
-	print "[-]", candidate
-	time.sleep(0.1)
-print "\nThe number of candidates = ", len(set(candidates))
+    candidatesFromPostLike = "https://graph.facebook.com/" + postID + "/likes?limit=1000&offset=0"
+    candidatesLikeJson = json.load(urllib2.urlopen(candidatesFromPostLike))
+    for candidatesLikeNumber in range(len(candidatesLikeJson["data"])):
+        nameOfCandidateLike = candidatesLikeJson["data"][candidatesLikeNumber]["name"]
+        candidatesLike.append(nameOfCandidateLike)
+
+    candidatesFromPostShare = "https://graph.facebook.com/" + postID + "/sharedposts?limit=1000&access_token=" + accessToken
+    candidatesShareJson = json.load(urllib2.urlopen(candidatesFromPostShare))
+    for candidatesShareNumber in range(len(candidatesShareJson["data"])):
+        nameOfCandidateShare = candidatesShareJson["data"][candidatesShareNumber]["from"]["name"]
+        candidatesShare.append(nameOfCandidateShare)
+
+    print "PostID = " + postID + " Like = " + str(len(candidatesLike)) + " Share = " + str(len(candidatesShare))
+    candidates = candidates + [candidate for candidate in candidatesShare if candidate in candidatesLike]
+
+time.sleep(4)
+for candidate2 in candidates:
+    print "[-]", candidate2
+    time.sleep(0.1)
+print "\nThe number of candidates = ", len(candidates)
 print "\nTrying to choose the winners\n"
 
 time.sleep(7)
